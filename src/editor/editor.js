98c8f1d52ad04456766da2a3a77d0f481bb7fd40
ghillairet <g.hillairet@gmail.com>
2012-10-12 15:02:00 +0200

    update basic editor.

--- src/editor/editor.js
+++ src/editor/editor.js
@@ -1,73 +1,195 @@
-$(function(){
+jQuery(function(){
 
-    var currentModel;
+    _.extend(Ecore.EObject.prototype, Backbone.Events);
 
-    $('#confirm-create-model').click(function(){
-        var modelName = $('#inputName').val();
-        var model = new Ecore.Model({ name: modelName });
-
-        currentModel = model;
-
-        $('#create-model-modal').modal('hide');
-
-
-
-
-        $('.editor').append(row);
+    $('[contenteditable]').live('focus', function() {
+        var $this = $(this);
+        $this.data('before', $this.html());
+        return $this;
+    }).live('blur keyup paste', function() {
+        var $this = $(this);
+        if ($this.data('before') !== $this.html()) {
+            $this.data('before', $this.html());
+            $this.trigger('change');
+        }
+        return $this;
     });
 
-    function createEPackage(parent) {
-        console.log(parent);
-        if (parent instanceof Ecore.Model) {
-          var ePackage = new Ecore.EPackage({ name: 'EPackage' });
-          parent.contents.push( ePackage );
-        };
-    };
+    var Editor = window.Editor = {};
 
-    function createDropDown(model) {
-        var dropdown = document.createElement('div');
-        dropdown.setAttribute('class', 'btn-group');
+    // Views
 
-        var dropdownToggle = document.createElement('a');
-        dropdownToggle.setAttribute('class', 'btn dropdown-toggle');
-        dropdownToggle.setAttribute('role', 'button');
-        dropdownToggle.setAttribute('data-toggle', 'dropdown');
-        dropdownToggle.setAttribute('href', '#');
-        dropdownToggle.innerHTML = '- resource: ' + modelName + ' ';
+    Editor.MetaBrowserView = Backbone.View.extend({
+        el: '#class-columns',
+        render: function() {
+            var packages = _.flatten(_.map(_.values(this.model.models), function(m) { return m.contents; }));
+            var view = new Editor.EModelElementColumnView({ model: packages, parent: this });
+            this.$el.append(view.render().$el);
+            return this;
+        }
+    });
 
-        var caret = document.createElement('span');
-        caret.setAttribute('class', 'caret');
+    Editor.EModelElementColumnView = Backbone.View.extend({
+        template: _.template('<div class="span4 outter-column"><div class="column"></div></div>'),
+        initialize: function(attributes) {
+            // model is an array
+            this.parent = attributes.parent;
+        },
+        render: function() {
+            var el = this.template();
+            this.setElement(el);
 
-        dropdownToggle.appendChild(caret);
-        dropdown.appendChild(dropdownToggle);
+            _.each(this.model, function(m) {
+                var view = new Editor.EModelElementRowView({ model: m });
+                $(this.$el.children()[0]).append( view.render().$el );
+            }, this);
 
-        var dropdownContent = document.createElement('ul');
-        dropdownContent.setAttribute('class', 'dropdown-menu');
-        dropdownContent.setAttribute('role', 'menu');
+            return this;
+        }
+    });
 
-        var dropdownContent1 = document.createElement('li');
+    Editor.EModelElementRowView = Backbone.View.extend({
+        template: _.template('<div class="row-fluid erow"><%= name %></div>'),
+        events: {
+            'mouseover': 'highlight',
+            'mouseout': 'unhighlight',
+            'click': 'showContent'
+        },
+        initialize: function(attributes) {
+            this.parent = attributes.parent;
+            this.contents = this.model.eContents();
+        },
+        render: function() {
+           var el = this.template(this.model.values);
+           this.setElement(el);
 
-        var action = document.createElement('a');
-        action.innerHTML = 'New EPackage';
-        action.addEventListener('click', function(evt) {
-            createEPackage(model);
-        });
+           return this;
+        },
+        highlight: function() {
+            this.$el.css({background: 'whitesmoke'});
+        },
+        unhighlight: function() {
+            this.$el.css({background: 'white'});
+        },
+        showContent: function() {
+            // this.highlight();
+            var parent = this.el.parentNode.parentNode.parentNode, // browser
+                child = parent.children, // columns
+                position = _.indexOf(child, this.el.parentNode.parentNode);
 
-        dropdownContent1.appendChild( action );
-        dropdownContent.appendChild( dropdownContent1 );
+            // removes the nexts.
+            for (var i = position + 1; i < child.length; i++) {
+                $(child[i]).remove();
+            }
 
-        dropdown.appendChild( dropdownContent );
+            if (this.contents.length > 0) {
+                this.contentView = new Editor.EModelElementColumnView({ model: this.contents });
+                this.contentView.render();
+                window.metaBrowser.$el.append(this.contentView.$el);
+            }
 
-        var row = document.createElement('div');
-        row.setAttribute('class', 'row-fluid');
+            if (window.currentProperty) {
+                window.currentProperty.$el.children().remove();
+            }
 
-        row.appendChild( dropdown );
+            if (!this.propertyView) {
+                this.propertyView = new Editor.PropertyView({ model: this.model });
+            }
 
-        return row;
-    };
+            this.propertyView.render();
+            window.currentProperty = this.propertyView;
 
-    function addAction(node, model) {
+            return this;
+        }
+    });
 
-    };
+    window.metaBrowser = new Editor.MetaBrowserView({model: Ecore.Registry});
+    window.metaBrowser.render();
+
+    Editor.PropertyRowView = Backbone.View.extend({
+        propertyTemplate: _.template('<tr><td><%= name %></td></tr>'),
+        valueTemplate: _.template('<td><div contenteditable><%= value %></div></td>'),
+        valueBooleanTemplate: _.template('<td><select><option>true</option><option>false</option></select></td>'),
+        render: function() {
+            var eFeature = this.model.eFeature,
+                model = this.model.eObject,
+                html = this.propertyTemplate({name: eFeature.get('name')}),
+                value, valueHtml;
+
+            this.setElement(html);
+
+            if (eFeature.get('eType') === Ecore.EcorePackage.EBoolean) {
+                value = model.get(eFeature.get('name'));
+                valueHtml = this.valueBooleanTemplate({value: value});
+
+                this.$el.append(valueHtml);
+
+                var select = $('td > select', this.$el);
+                select.val(value); // init select
+
+                select.change(function(){
+                    var changed = $('option:selected', select).val() === 'true';
+                    model.set(eFeature.get('name'), changed == 'true');
+                    // model.trigger('change');
+                });
+            } else {
+                value = model.get(eFeature.get('name'));
+                valueHtml = this.valueTemplate({ value: value});
+                this.$el.append(valueHtml);
+
+                var div = $('td > div', this.$el);
+                div.on('change', function() {
+                    console.log(div.text());
+                });
+            }
+            return this;
+        }
+    });
+
+    Editor.PropertyView = Backbone.View.extend({
+        el: '#property-view',
+        template: _.template('<table class="table table-striped"><thead><tr><th style="width: 30%"></th><th style="width: 70%"></th></tr></thead><tbody></tbody></table>'),
+        initialize: function(attributes) {},
+        render: function() {
+            this.$el.children().remove();
+            var html = this.template();
+            this.$el.append(html);
+
+            var attrs = _.filter(this.model.eClass.eAllStructuralFeatures(), function(f) {
+                return f.isTypeOf('EAttribute') && this.isSet(f.get('name'));
+            }, this.model);
+
+            _.each(attrs, function(attr) {
+                var view = new Editor.PropertyRowView({
+                    model: {
+                        eFeature: attr,
+                        eObject: this.model
+                    }
+                });
+                view.render();
+                $('table > tbody', this.$el).append(view.$el);
+            }, this);
+
+            return this;
+        }
+    });
+
+    // Models
+
+    Editor.ResourceModel = Backbone.Model.extend({
+
+        initialize: function(attributes) {
+
+        },
+
+        parse: function() {
+
+        },
+
+        toJSON: function() {
+
+        }
+
+    });
 
 });
