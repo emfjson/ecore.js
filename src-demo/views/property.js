138867239fa1054dcfdfd41f044aeac7c150ec19
ghillairet <g.hillairet@gmail.com>
2012-12-19 14:33:14 +0100

    removed gh-pages stuff

--- src-demo/views/property.js
+++
@@ -1,8 +0,0 @@
-
-var PropertyWindow = Ecore.Editor.Window.extend({
-    el: '#property-window',
-    title: 'Property',
-    draggable: true,
-    content: new Ecore.Editor.PropertySheetView()
-});
-
