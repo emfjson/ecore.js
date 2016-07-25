ab1a9ca3070803d68ce48f431f3b383c8caf109e
dphillips <dphillips@CohesionForce.com>
2016-07-25 15:07:47 -0500

    Adding example with ecore file that was generated in Eclipse EMF.

---
+++ examples/xmi_node/README.md
@@ -0,0 +1,25 @@
+# XMI Node Example 
+This example includes the loading of the ecore file and then loading 
+two sample files built with that EMF model.
+
+The EMF Model is found in TestModel.ecore.  Note that this file is an XMI formated file.
+
+The test1.testmodel file conforms to the TestModel ecore file format and includes a reflexive relationship with subclasses.  Note that the parent Item is not abstract.
+
+The test2.testmodel file conforms to the TestModel ecore file format and includes a simple composite structure that is nested two classes deep.
+
+## To run with only the ecore type:
+
+node simpleTest.js TestModel.ecore
+
+## To run with the ecore and the first test model type: 
+
+node simpleTest.js TestModel.ecore test1.testmodel test2.testmodel 
+
+## To run with the ecore and the second test model type: 
+
+node simpleTest.js TestModel.ecore test2.testmodel 
+
+## To run with the ecore and both test models type: 
+
+node simpleTest.js TestModel.ecore test1.testmodel test2.testmodel 
---
+++ examples/xmi_node/simpleTest.js
@@ -0,0 +1,72 @@
+//////////////////////////////////////////////////////////////////////////
+//
+//////////////////////////////////////////////////////////////////////////
+
+fs = require('fs');
+util = require('util');
+//Ecore = require('ecore/dist/ecore.xmi');
+Ecore = require('../../dist/ecore.xmi');
+
+//////////////////////////////////////////////////////////////////////////
+//
+//////////////////////////////////////////////////////////////////////////
+function displayModelInfo(model) {
+
+  // Note: This function is extracted from the callback function from the 
+  // main.js in the node example
+
+  var ePackage = model.get('contents').first();
+
+  console.log('loaded ePackage', ePackage.get('name'));
+  console.log('eClassifiers', ePackage.get('eClassifiers').map(function(c) {
+      return c.get('name') + ' superTypes(' + c.get('eSuperTypes').map(function(s) {
+          return s.get('name');
+      }).join(', ') + ') features(' + c.get('eStructuralFeatures').map(function(f) {
+          return f.get('name') + ' : ' + f.get('eType').get('name');
+      }).join(', ') + ')';
+  }));  
+}
+
+//////////////////////////////////////////////////////////////////////////
+//
+//////////////////////////////////////////////////////////////////////////
+function processFile(file) {
+  var resourceSet = Ecore.ResourceSet.create();
+  var resource = resourceSet.create({uri : 'uri-' + file});
+
+  var fileContents = fs.readFileSync(file, 'utf8');
+
+  try { 
+    resource.parse(fileContents, Ecore.XMI);
+  } catch(err) {
+    console.log('*** Failed parsing file: ' + file);
+    console.trace(err);
+    return;
+  }
+
+  var firstElement = resource.get('contents').first();
+  if(firstElement.eClass.values.name === 'EPackage') {
+    // This is an EPackage, so add it to the registry
+    console.log("::: Adding to registry: " + firstElement.get('name'));
+    Ecore.EPackage.Registry.register(firstElement);
+    console.log("::: Display some model information");
+    displayModelInfo(resource);
+  }
+	
+  console.log("::: JSON Dump of " + file);
+  console.log(util.inspect(resource.to(Ecore.JSON), false, null));
+  
+  console.log("::: XMI Dump of " + file);
+  console.log(resource.to(Ecore.XMI, true));
+}
+
+//////////////////////////////////////////////////////////////////////////
+//  Main Processing
+//////////////////////////////////////////////////////////////////////////
+
+for(var argidx = 2; argidx < process.argv.length; argidx++) {
+  // Process each file that is passed on the command line
+  var fileName = process.argv[argidx];
+  console.log('::: Processing ' + fileName);
+  processFile(fileName);
+}
